e65c52fbb58cbb64bb9a51ba5f6ddb982fe53da8
Alex181945 <alestra55@hotmail.com>
2018-05-18 17:21:01 -0500

    Se agregan los servicios para el consumo del servicio rest para los estados de tickets

---
+++ src/modelos/ct/estatus_ticket/estatus_ticketM.js
@@ -0,0 +1,210 @@
+/**
+ * 
+ * Autor: Alejandro Estrada
+ * Fecha: 15/04/2018
+ * Descripcion: Modelo Edificios
+ * REST
+ * 
+ * Modificaciones:
+ * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
+ * Ejemplo: Alejandro Estrada 09/09/2017 In-15 Fn-19 
+ * 
+ */
+
+'use strict'
+
+const claseConexion = require(__basedir + 'db/conexion'),
+     EstatusTicketModelo  = () => {};
+const traeResultado = require(__basedir + 'src/servicios/funciones/funciones');     
+
+EstatusTicketModelo.todos = (data, callback) => {
+    
+    /*Instancia de clase conexion*/
+    let conexion = new claseConexion();
+   
+    /*Ejecucion de metodo conectar*/
+    let consulta = conexion.conectar();
+
+    /*Respuesta consulta*/
+    let respuesta;
+
+    /*Procedimiento MySql*/
+    let sql = `SET @p0 = '${data}'; `
+    sql += 'CALL consultaEstatusTickets(@p0, @p1, @p2, @p3);';
+    sql += 'SELECT @p1 AS `lError`, @p2 AS `cSqlState`, @p3 AS `cError`;';
+
+    /*Llamado de un query haciendo uso de una funcion*/
+    consulta.query(sql, function(error, result, fields){
+
+        /*Variable que guarda el resultado*/
+        let resultado;
+
+        /*Llenado del resultado*/
+        if(error){
+            resultado = {
+                error: error
+            }    
+        } else{
+            resultado = traeResultado.leeResultadoProcedimiento(result);
+        }
+        /*Solucion a la callback*/
+        callback(resultado);
+    });
+   
+    /*Ejecucion de metodo desconectar*/
+    conexion.desconectar();
+
+};
+
+EstatusTicketModelo.uno = (data, callback) => {
+
+    /*Instancia de clase conexion*/
+    let conexion = new claseConexion();
+   
+    /*Ejecucion de metodo conectar*/
+    let consulta = conexion.conectar();
+
+    /*Procedimiento MySql*/
+    let sql = `SET @p0 = '${data}'; `;
+    sql += 'CALL consultaEstatusTicket(@p0, @p1, @p2, @p3);';
+    sql += 'SELECT @p1 AS `lError`, @p2 AS `cSqlState`, @p3 AS `cError`;';
+
+    consulta.query(sql, function(error, result, fields){
+
+        /*Variable que guarda el resultado*/
+        let resultado;
+
+        /*Llenado del resultado*/
+        if(error){
+            resultado = {
+                error: error
+            }    
+        } else{
+            resultado = traeResultado.leeResultadoProcedimiento(result);            
+        }
+        /*Solucion a la callback*/
+        callback(resultado);
+    });
+   
+    /*Ejecucion de metodo desconectar*/
+    conexion.desconectar();
+
+};
+
+EstatusTicketModelo.inserta = (data, callback) => {
+
+    /*Instancia de clase conexion*/
+    let conexion = new claseConexion();
+   
+    /*Ejecucion de metodo conectar*/
+    let consulta = conexion.conectar();
+
+    /*Conversion de string a json*/
+    let obj = JSON.parse(data);
+
+    /*Procedimiento MySql*/
+    let sql = `SET @p0 = '${obj.cEstado}'; SET @p1 = '${obj.cUsuario}'; `;
+    sql += 'CALL insertaEstatusTicket(@p0, @p1, @p2, @p3, @p4);';
+    sql += 'SELECT @p2 AS `lError`, @p3 AS `cSqlState`, @p4 AS `cError`;';
+
+    /*Llamado de un query haciendo uso de una funcion*/
+    consulta.query(sql, function(error, result, fields){
+
+        /*Variable que guarda el resultado*/
+        let resultado;
+
+        /*Llenado del resultado*/
+        if(error){
+            resultado = {
+                error: error
+            }    
+        } else{
+            resultado = traeResultado.leeResultadoProcedimiento(result);            
+        }
+        /*Solucion a la callback*/
+        callback(resultado);
+    });
+   
+    /*Ejecucion de metodo desconectar*/
+    conexion.desconectar();
+
+};
+
+EstatusTicketModelo.actualiza = (data, callback) => {
+
+    /*Instancia de clase conexion*/
+    let conexion = new claseConexion();
+   
+    /*Ejecucion de metodo conectar*/
+    let consulta = conexion.conectar();
+
+    /*Conversion de string a json*/
+    let obj = JSON.parse(data);
+
+    /*Procedimiento MySql*/
+    let sql = `SET @p0 = '${obj.iIDEstado}'; `; 
+    sql += `SET @p1 = '${obj.cEstado}'; SET @p2 = '${obj.lActivo}'; `;
+    sql += `SET @p3 = '${obj.cUsuario}';`;
+    sql += 'CALL actualizaEstatusTicket(@p0, @p1, @p2, @p3, @p4, @p5, @p6);';
+    sql += 'SELECT @p4 AS `lError`, @p5 AS `cSqlState`, @p6 AS `cError`;';
+
+    /*Llamado de un query haciendo uso de una funcion*/
+    consulta.query(sql, function(error, result, fields){
+
+        /*Variable que guarda el resultado*/
+        let resultado;
+
+        /*Llenado del resultado*/
+        if(error){
+            resultado = {
+                error: error
+            }    
+        } else{
+            resultado = traeResultado.leeResultadoProcedimiento(result);            
+        }
+        /*Solucion a la callback*/
+        callback(resultado);
+    });
+   
+    /*Ejecucion de metodo desconectar*/
+    conexion.desconectar();
+
+};
+
+EstatusTicketModelo.borra = (data, callback) => {
+
+    /*Instancia de clase conexion*/
+    let conexion = new claseConexion();
+   
+    /*Ejecucion de metodo conectar*/
+    let consulta = conexion.conectar();
+
+    /*Procedimiento MySql*/
+    let sql = `SET @p0 = '${data.iIDEstado}'; SET @p1 = '${data.cUsuario}'; `; 
+    sql += 'CALL borraEstatusTicket(@p0, @p1, @p2, @p3, @p4);';
+    sql += 'SELECT @p2 AS `lError`, @p3 AS `cSqlState`, @p4 AS `cError`;';
+
+    /*Llamado de un query haciendo uso de una funcion*/
+    consulta.query(sql, function(error, result, fields){
+
+        /*Variable que guarda el resultado*/
+        let resultado;
+
+        /*Llenado del resultado*/
+        if(error){
+            resultado = {
+                error: error
+            }    
+        } else{
+            resultado = traeResultado.leeResultadoProcedimiento(result);            
+        }
+        /*Solucion a la callback*/
+        callback(resultado);
+    });
+   
+    /*Ejecucion de metodo desconectar*/
+    conexion.desconectar();
+
+};
+
+module.exports = EstatusTicketModelo;
---
+++ src/rutas/ct/estatus_ticket/estatus_ticketR.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Autor: Alejandro Estrada
+ * Fecha: 18/05/2018
+ * Descripcion: Maneja las rutas del catalogo del estatus del ticket
+ *  
+ * Modificaciones:
+ * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
+ * 
+ */
+
+'use strict'
+
+const EstatusTicketControlador = require(__basedir + 'src/controladores/ct/estatus_ticket/estatus_ticketC'),
+    express = require('express'),
+    auth    = require(__basedir + 'src/middleware/auth'),
+    router  = express.Router();
+    
+    router
+      .get('/estatus-ticket/consulta', auth, EstatusTicketControlador.todos)
+      .post('/estatus-ticket/inserta', auth, EstatusTicketControlador.inserta)
+      .get('/estatus-ticket/consulta/uno', auth, EstatusTicketControlador.uno)
+      .put('/estatus-ticket/actualiza', auth, EstatusTicketControlador.actualiza)
+      .delete('/estatus-ticket/borra', auth, EstatusTicketControlador.borra)
+
+module.exports = router;
