f06d1a4c4ad6bc9d70500a580b5fc95f1253dded
Alex181945 <alestra55@hotmail.com>
2020-01-07 10:32:09 -0600

    Pruebas Archivo por archivo

--- src/controladores/op/ticket/ticketC.js
+++ src/controladores/op/ticket/ticketC.js
@@ -1,77 +1,80 @@
 /**
- * 
+ *
  * Autor: Alejandro Estrada
  * Fecha: 07/04/2018
  * Descripcion: Controlador Carga datos de la aplicacion
  * en funcion al perfil del usuario
- * 
+ *
  * Modificaciones:
  * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
- * Ejemplo: Alejandro Estrada 09/09/2017 In-15 Fn-19 
- * 
+ * Ejemplo: Alejandro Estrada 09/09/2017 In-15 Fn-19
+ *
  */
 
-'use strict'
+"use strict";
 
- const TicketModelo = require(__basedir + 'src/modelos/op/ticket/ticketM'),
-      TicketControlador = () => {};
-const AsignaTecnico = require(__basedir + 'src/servicios/funciones/asignaTecnico');     
+const TicketModelo = require(__basedir + "src/modelos/op/ticket/ticketM"),
+  TicketControlador = () => {};
+const AsignaTecnico = require(__basedir +
+  "src/servicios/funciones/asignaTecnico");
 
- TicketControlador.todos = (req, res, next) => {
+TicketControlador.todos = (req, res, next) => {
+  let consulta = {
+    iTipoConsulta: req.body.iTipoConsulta,
+    iIDPerfil: req.body.iIDPerfil
+  };
 
-    let consulta = {
-        iTipoConsulta  : req.body.iTipoConsulta,
-        iIDPerfil : req.body.iIDPerfil
+  TicketModelo.todos(consulta, (error, filas) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(filas);
     }
+  });
+};
 
-    TicketModelo.todos(consulta, (error, filas) => {
-        if(error){
-            res.send(error);
-        } else {
-            res.send(filas);
+TicketControlador.uno = (req, res, next) => {};
+
+TicketControlador.inserta = (req, res, next) => {
+  /*Se invoca al asigna tecnico de manera automatica*/
+  /*Prueba de cambios archivo por archivo */
+  /*Mas Pruebas */
+  switch (req.body.iTipoAsignacionTecnico) {
+    case "1":
+      AsignaTecnico.cargaDeTrabajo(
+        req.body.iTipoAsignacionTecnico,
+        (error, filas) => {
+          if (error) {
+            console.log("Entro: " + error);
+            req.body.iTipoAsignacionTecnico = error;
+          } else {
+            req.body.iTipoAsignacionTecnico = filas;
+          }
         }
-    });
- };
+      );
+      break;
+    default:
+      console.log("No se indico ningun tipo de asignacion de tecnico");
+      break;
+  }
 
- TicketControlador.uno = (req, res, next) => {};
+  console.log(req.body.iTipoAsignacionTecnico);
+  let ticket = {
+    objTicket: req.body.objTicket,
+    iIDTecnico: req.body.iTipoAsignacionTecnico
+  };
 
- TicketControlador.inserta = (req, res, next) => {
+  TicketModelo.inserta(ticket, (error, filas) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(filas);
+    }
+  });
+};
 
-    /*Se invoca al asigna tecnico de manera automatica*/
-     switch (req.body.iTipoAsignacionTecnico){
-        case '1' :
-             AsignaTecnico.cargaDeTrabajo(req.body.iTipoAsignacionTecnico, (error, filas) =>{
-                if(error){
-                    console.log("Entro: " + error);
-                    req.body.iTipoAsignacionTecnico = error;
-                } else {
-                    req.body.iTipoAsignacionTecnico = filas;
-                }
-             });
-        break;
-        default:
-            console.log('No se indico ningun tipo de asignacion de tecnico');
-        break;
-     }
+TicketControlador.actualiza = (req, res, next) => {};
 
-     console.log(req.body.iTipoAsignacionTecnico);
-     let ticket ={
-         "objTicket" : req.body.objTicket,
-         "iIDTecnico" : req.body.iTipoAsignacionTecnico
-     }
-     
-     TicketModelo.inserta(ticket, (error, filas) =>{
-        if(error){
-            res.send(error);
-        } else {
-            res.send(filas);
-        }
-     });
-     
- };
+TicketControlador.borra = (req, res, next) => {};
 
- TicketControlador.actualiza = (req, res, next) => {};
-
- TicketControlador.borra = (req, res, next) => {};
-
- module.exports = TicketControlador;
+module.exports = TicketControlador;
